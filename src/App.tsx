330f0db6b6135750c521f3bf03dd08a88904e627
unknown <shafi.2013@gmail.com>
2025-05-17 01:47:28 +0600

    Waving flag added with the moon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 // src/App.tsx
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useRef } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { Stars, OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
+
+function WavingFlag() {
+  const mesh = useRef<THREE.Mesh>(null)
+  const texture = useLoader(THREE.TextureLoader, '/bangladesh-flag.png') // Place image in public/
+
+  useFrame(({ clock }) => {
+    const t = clock.getElapsedTime()
+    if (mesh.current) {
+      const geometry = mesh.current.geometry as THREE.PlaneGeometry
+      const position = geometry.attributes.position
+      for (let i = 0; i < position.count; i+=1) {
+        const x = position.getX(i)
+        const y = position.getY(i)
+        // Waving effect: amplitude increases with y (top of flag waves more)
+        const wave = Math.sin(x * 30 + t * 4) * 0.01 * (y + 1)
+        position.setZ(i, wave)
+      }
+      position.needsUpdate = true
+      // geometry.computeVertexNormals()
+    }
+  })
+
+  return (
+    <group position={[0.045, 0.07, 0]}>
+      <mesh ref={mesh} position={[0, 0, 0]}>
+      <planeGeometry args={[0.08, 0.05, 16, 8]} />
+      <meshStandardMaterial map={texture}
+        side={THREE.DoubleSide}
+        transparent={false} />
+    </mesh>
+      {/* Red circle on flag */}
+          {/* <mesh position={[0, 0, 0.001]}>
+            <circleGeometry args={[0.015, 32]} />
+            <meshStandardMaterial color="#f42a41" />
+          </mesh> */}
+    </group>
+    
+  )
+}
 
 function App() {
   return (
@@ -32,6 +72,16 @@ function App() {
           <sphereGeometry args={[1, 64, 64]} />
           <meshStandardMaterial color="#cccccc" roughness={1} metalness={0.2} />
         </mesh>
+        {/* Bangladesh flag on the moon */}
+        <group position={[0.2, 1.01, 0]}>
+          {/* Flag pole */}
+          <mesh>
+            <boxGeometry args={[0.01, 0.2, 0.01]} />
+            <meshStandardMaterial color="#888888" />
+          </mesh>
+          {/* Flag */}
+          <WavingFlag />
+        </group>
         <OrbitControls enableZoom={true} autoRotate autoRotateSpeed={0.1} />
       </Canvas>
     </div>
